9ae59dc3b3dedde6858aab2ed0e17d38cc55ec8e
Kurino <gejingze@163.com>
2022-03-22 19:39:30 +0800

    add: naive ui config

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,17 @@
 <template>
-   <div class="logo-box">
-    <img style="height:140px;" alt="Vue logo" src="./assets/vue.png" />
-    <span/> 
-    <img style="height:140px;" alt="Vite logo" src="./assets/vite.svg" />
-    <span/>
-    <img style="height:140px;" alt="NaiveUI logo" src="./assets/naiveUI.svg">
-  </div>
-  <HelloWorld msg="Hello Vue 3 + NaiveUI + Vite + TypeScript" />
-  <div class="static-public">
-    Place static files into the <code>src/components</code> folder
-    <img style="width:90px;" src="./assets/node.png" />
-  </div>
+   <div class="flex-y-center h-full">
+     <n-button type="primary" @click="num++">
+       数字是：{{ num }}
+     </n-button>
+     <n-button>
+       数字+1：{{ num + 1 }}
+     </n-button>
+   </div>
 </template>
 
 <script setup lang="ts">
-  import HelloWorld from './components/HelloWorld.vue';
+  import { ref } from 'vue';
+  const num = ref(0)
 </script>
 
-<style scoped>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-.logo-box {
-  display: flex;
-  width: 100%;
-  justify-content: center;
-}
-
-.logo-box span {
-  width: 74px;
-}
-
-.static-public {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.static-public code {
-  background-color: #eee;
-  padding: 2px 4px;
-  margin: 0 4px;
-  border-radius: 4px;
-  color: #304455;
-}
-</style>
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { createApp } from 'vue';
-
+import { setupNaiveUI } from './plugins';
 import App from './App.vue';
 
 async function setupApp() {
   // 引入静态资源
   const app = createApp(App);
+  setupNaiveUI(app)
   // 路由准备就绪后挂载 App
   app.mount('#app');
 }
