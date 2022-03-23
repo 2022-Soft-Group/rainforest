88cc3ab719373db29a6a587112796e3c2a2900b7
Kurino <gejingze@163.com>
2022-03-23 17:27:05 +0800

    fix: config windicss

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
-import { createApp } from 'vue';
-import { setupNaiveUI } from './plugins';
-import App from './App.vue';
+import { createApp } from "vue";
+import { setupNaiveUI, setupAssets } from "./plugins";
+import App from "./App.vue";
 
 async function setupApp() {
   // 引入静态资源
+  setupAssets();
   const app = createApp(App);
-  setupNaiveUI(app)
+  setupNaiveUI(app);
   // 路由准备就绪后挂载 App
-  app.mount('#app');
+  app.mount("#app");
 }
 
 setupApp();
