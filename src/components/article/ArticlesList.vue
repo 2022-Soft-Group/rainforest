a48d7debc653d042f7d6cd2e44f22119360b8553
Kurino <gejingze@163.com>
2022-04-10 19:35:44 +0800

    fix: adjust load structure

--- src/components/article/ArticlesList.vue
+++ src/components/article/ArticlesList.vue
@@ -1,35 +1,12 @@
 <template>
-  <n-card :bordered="false" class="flex-auto m-4 w-180 rounded-10px shadow-sm">
-    <n-tabs type="line" size="large" class="mb-6">
-      <n-tab name="推荐">推荐</n-tab>
-      <n-tab name="关注">关注</n-tab>
-    </n-tabs>
-    <div v-for="(item, index) in articles">
-      <n-divider v-if="index != 0"></n-divider>
-      <articles-list-item :is-loading="isLoading" :article-info="item" />
-    </div>
-  </n-card>
+  <div v-for="(item, index) in articles">
+    <n-divider v-if="index != 0"></n-divider>
+    <articles-list-item :is-loading="isLoading" :article-info="item" />
+  </div>
 </template>
 
 <script setup lang="ts">
-import { getArticleListRecommand } from '@/api/article';
-import { ref, onMounted } from 'vue';
-
-// const props = defineProps<{ articles: Array<ArticlesListItem> }>();
-const isLoading = ref(false);
-const articles = ref<Array<ArticlesListItem>>([]);
-
-function reload() {
-  getArticleListRecommand().then((res) => {
-    if (res.data.status == 0) {
-      articles.value = res.data.data.articleInfo as Array<ArticlesListItem>;
-    } else {
-      window.$message.error('获取推荐列表失败');
-    }
-  });
-}
-
-onMounted(reload);
+const props = defineProps<{ articles: Array<ArticlesListItem>; isLoading: boolean }>();
 </script>
 
 <style scoped></style>
--- src/views/homepage/HomePage.vue
+++ src/views/homepage/HomePage.vue
@@ -1,7 +1,31 @@
 <template>
-  <articles-list />
+  <n-card :bordered="false" class="flex-auto m-4 w-180 rounded-10px shadow-sm">
+    <n-tabs type="line" size="large" class="mb-6">
+      <n-tab name="推荐">推荐</n-tab>
+      <n-tab name="关注">关注</n-tab>
+    </n-tabs>
+    <articles-list :articles="articles" :is-loading="isLoading" />
+  </n-card>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { getArticleListRecommand } from '@/api/article';
+import { ref, onMounted } from 'vue';
+
+function reload() {
+  isLoading.value = true;
+  getArticleListRecommand().then((res) => {
+    if (res.data.status == 0) {
+      articles.value = res.data.data.articleInfo as Array<ArticlesListItem>;
+      isLoading.value = false;
+    } else {
+      window.$message.error('获取推荐列表失败');
+    }
+  });
+}
+const isLoading = ref(false);
+const articles = ref<Array<ArticlesListItem>>([]);
+onMounted(reload);
+</script>
 
 <style scoped></style>
