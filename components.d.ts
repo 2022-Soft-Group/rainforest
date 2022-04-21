a01009e5c0e3a25e4044ab213b120acad5cde12b
Kurino <gejingze@163.com>
2022-04-21 10:52:53 +0800

    fix: regulize all green

--- components.d.ts
+++ components.d.ts
@@ -6,17 +6,17 @@ declare module 'vue' {
   export interface GlobalComponents {
     ArticlesList: typeof import('./src/components/article/ArticlesList.vue')['default']
     ArticlesListItem: typeof import('./src/components/article/ArticlesListItem.vue')['default']
-    AvatarDropdown: typeof import('./src/components/common/globalheader/AvatarDropdown.vue')['default']
+    AvatarDropdown: typeof import('./src/components/common/GlobalHeader/AvatarDropdown.vue')['default']
     GlobalContent: typeof import('./src/components/common/globalcontent/GlobalContent.vue')['default']
-    GlobalHeader: typeof import('./src/components/common/globalheader/GlobalHeader.vue')['default']
+    GlobalHeader: typeof import('./src/components/common/GlobalHeader/GlobalHeader.vue')['default']
     HoverContainer: typeof import('./src/components/common/HoverContainer.vue')['default']
-    MessageDropdown: typeof import('./src/components/common/globalheader/MessageDropdown.vue')['default']
+    MessageDropdown: typeof import('./src/components/common/GlobalHeader/MessageDropdown.vue')['default']
     NaiveProvider: typeof import('./src/components/common/NaiveProvider.vue')['default']
     QuickGuider: typeof import('./src/components/common/globalcontent/QuickGuider.vue')['default']
     RankingList: typeof import('./src/components/common/globalcontent/RankingList.vue')['default']
     SignInForm: typeof import('./src/components/login/SignInForm.vue')['default']
     SignUpForm: typeof import('./src/components/login/SignUpForm.vue')['default']
-    TrendDropdown: typeof import('./src/components/common/globalheader/TrendDropdown.vue')['default']
+    TrendDropdown: typeof import('./src/components/common/GlobalHeader/TrendDropdown.vue')['default']
     UploadButton: typeof import('./src/components/common/UploadButton.vue')['default']
   }
 }
