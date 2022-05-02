afcc6bb43853262b63b4ec02fc8a25d44a0c599a
Kurino <gejingze@163.com>
2022-05-02 18:42:57 +0800

    refactor: change comment component dir

---
+++ src/components/comments/CommentListItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <router-link :to="'/user/' + userInfo.id">
+    <n-thing>
+      <template #avatar>
+        <n-avatar round :src="userInfo.avatar"></n-avatar>
+      </template>
+      <template #header>{{ userInfo.name }}</template>
+      <template #description> {{ userInfo.description }} </template>
+    </n-thing>
+  </router-link>
+</template>
+
+<script setup lang="ts">
+import { getUserInfo } from '@/api/user';
+import { onMounted, ref } from 'vue';
+const userInfo = ref<User>({
+  name: '',
+  description: '',
+  avatar: '',
+  id: 0,
+  sex: '',
+  email: '',
+  phone: '',
+  coin: 0,
+  createTime: '',
+  modifyTime: '',
+});
+
+onMounted(getUserInfo());
+</script>
