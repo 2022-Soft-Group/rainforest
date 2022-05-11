cd530802a9f8ed72f52788256bf44dc1f668e4bf
rainy-song <916367062@qq.com>
2022-05-11 21:40:10 +0800

    ColumnListItem

---
+++ src/components/column/ColumnList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="ColumnHomeRecommendation-cardContainer">
+    <div>
+      <column-list-item></column-list-item>
+    </div>
+    <div>
+      <column-list-item></column-list-item>
+    </div>
+    <div>
+      <column-list-item></column-list-item>
+    </div>
+    <div>
+      <column-list-item></column-list-item>
+    </div>
+    <div>
+      <column-list-item></column-list-item>
+    </div>
+    <div>
+      <column-list-item></column-list-item>
+    </div>
+    <div>
+      <column-list-item></column-list-item>
+    </div>
+    <div>
+      <column-list-item></column-list-item>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.ColumnHomeRecommendation-cardContainer {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
+}
+</style>
---
+++ src/typings/column.d.ts
@@ -0,0 +1,8 @@
+type ColumnListItem = {
+  href: string;
+  imgSrc: string;
+  title: string;
+  description: string;
+  followerNum: number;
+  ArticleNum: number;
+};
