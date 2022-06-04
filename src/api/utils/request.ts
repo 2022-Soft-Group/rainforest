f20ecb41dd703a8c1e6c1dfd470620d033f03a55
Gaspard <gaspard.ruan@gmail.com>
2022-06-04 09:55:30 +0800

    change url

--- src/api/utils/request.ts
+++ src/api/utils/request.ts
@@ -3,10 +3,10 @@ import qs from 'qs';
 const mockUrl = 'http://127.0.0.1:4523/mock/836630';
 const url = 'http://kurino.top/api';
 const backend = axios.create({
-  baseURL: mockUrl,
+  baseURL: url,
 });
 const jsonBackend = axios.create({
-  baseURL: mockUrl,
+  baseURL: url,
 });
 
 backend.interceptors.request.use((config) => {
