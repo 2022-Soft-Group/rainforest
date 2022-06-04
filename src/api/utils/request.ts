be33be9fc09a1d30d432673ca0c22d2192204196
Gaspard <gaspard.ruan@gmail.com>
2022-06-04 17:48:03 +0800

    FollowButton update

--- src/api/utils/request.ts
+++ src/api/utils/request.ts
@@ -3,10 +3,10 @@ import qs from 'qs';
 const mockUrl = 'http://127.0.0.1:4523/mock/836630';
 const url = 'http://kurino.top/api';
 const backend = axios.create({
-  baseURL: url,
+  baseURL: mockUrl,
 });
 const jsonBackend = axios.create({
-  baseURL: url,
+  baseURL: mockUrl,
 });
 
 backend.interceptors.request.use((config) => {
