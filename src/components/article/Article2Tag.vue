b3d072020447edc41560a46f6f4866fa4df12955
rainy-song <916367062@qq.com>
2022-06-07 01:37:50 +0800

    fix component

---
+++ src/components/article/Article2Tag.vue
@@ -0,0 +1,55 @@
+<template>
+  <n-select
+    v-model:value="multipleSelectValue"
+    v-if="isTag"
+    filterable
+    multiple
+    tag
+    :options="options"
+    clearable
+    :consistent-menu-width="false"
+  />
+</template>
+<script setup lang="ts">
+import { getSections, getTags } from '@/api/sections';
+import type { SelectOption } from 'naive-ui';
+import { onMounted, ref } from 'vue';
+const props = defineProps<{ isTag: boolean }>();
+
+const multipleSelectValue = ref([]);
+const options: { label: string; value: string; type: 'group'; children: Array<SelectOption> }[] = [];
+onMounted(() => {
+  reload();
+});
+function reload() {
+  getSections().then((res) => {
+    if (res.data.status == 0) {
+      res.data.data.sections.forEach((elm: string) => {
+        const optionsChildren: { label: string; value: number }[] = [];
+        getTags({ sectionName: elm as string, size: 2, page: 0 }).then((res) => {
+          if (res.data.status == 0) {
+            res.data.data.tags.forEach((elen: TagItem) => {
+              optionsChildren.push({
+                label: elen.title,
+                value: elen.id,
+              });
+            });
+          } else {
+            window.$message.error('获取二级列表失败');
+          }
+        });
+        ////////
+        options.push({
+          label: elm,
+          value: elm,
+          type: 'group',
+          children: optionsChildren,
+        });
+      });
+    } else {
+      window.$message.error('获取Section失败');
+    }
+  });
+}
+</script>
+<style></style>
