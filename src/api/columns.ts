402357f8ca8b4d0747624fa46c12f44ae2d76479
kurino <gejingze@163.com>
2022-06-08 11:08:52 +0800

    fix: add back columns api

--- src/api/columns.ts
+++ src/api/columns.ts
@@ -9,7 +9,7 @@ export function getColumnListRecommand(data: { size: number; page: number }) {
 }
 
 export function getColumnDetail(id: string) {
-  return backend.get(`/column/${id}`);
+  return backend.get(`column/${id}`);
 }
 
 export function addColumn(column: ColumnUpload) {
@@ -17,12 +17,13 @@ export function addColumn(column: ColumnUpload) {
 }
 
 export function getColumnArticleList(data: { size: number; page: number; columnID: number }) {
-  return backend.get(`/column/${data.columnID}/articles`, { params: data });
+  return backend.get(`column/${data.columnID}/articles`, { params: data });
 }
 
-export function deleteColumn(id: number) {
-  return backend.delete(`/column/${id}`);
+export function getMyColumns(data: { size: number; page: number }) {
+  return backend.get(`column/list`, { params: data });
 }
-export function collectColumn(columnID: string) {
-  return backend.put(`/column/${columnID}/follow`);
+
+export function getUserColumns(userID: number, data: { size: number; page: number }) {
+  return backend.get(`column/list/${userID}`, { params: data });
 }
