c940d00c3e7711d9b4f5de7cbc01311cc18c2a68
kurino <gejingze@163.com>
2022-06-09 00:39:50 +0800

    fix: admin storage

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -3,20 +3,22 @@ import { defineStore } from 'pinia';
 
 export const useAuthStore = defineStore('auth', () => {
   const token = ref(localStorage.getItem('token') || '');
+  const admin = ref(localStorage.getItem('isAdmin') || '');
   const isLogin = computed(() => {
     return token.value !== '';
   });
-  const isAdmin = ref(false);
-
+  const isAdmin = computed(() => {
+    return admin.value == 'true';
+  });
   function signIn(userToken: string, ID: number, userAvatar: string, admin: boolean) {
     localStorage.setItem('token', userToken);
     localStorage.setItem('userID', ID.toString());
     localStorage.setItem('avatar', userAvatar);
-    isAdmin.value = admin;
+    localStorage.setItem('isAdmin', admin ? 'true' : 'false');
   }
   function signOut() {
     localStorage.clear();
   }
 
-  return { isLogin, signIn, signOut };
+  return { isLogin, isAdmin, signIn, signOut };
 });
